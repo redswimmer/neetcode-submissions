8a618d80b55e4f8aa06cc23c96205cb566a4a74c
Andrew Savala <38088099+redswimmer@users.noreply.github.com>
2026-07-11 17:28:10 -0700

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key, value):
+        self.key, self.val = key, value
+        self.next = self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.left, self.right = Node(0, 0), Node(0, 0) # left = LRU, right = MRU
+        self.left.next, self.right.prev = self.right, self.left
+
+        self.cache = {} # Node pointer
+
+    # Removes a node from the list
+    def remove(self, node: Node) -> None:
+        print(f"Remove {node.key}")
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # Inserts a node to the list on most right (MRU)
+    def insert_right(self, node: Node) -> None:
+        print(f"Insert {node.key}")
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+        self.cache[node.key] = node
+
+        print(f"cache {[key for key in self.cache]}")
+
+    
+    def get(self, key: int) -> int:
+        if not key in self.cache:
+            return -1
+        
+        self.remove(self.cache[key])
+        self.insert_right(self.cache[key])
+        return self.cache[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        node = Node(key, value)
+        self.insert_right(node)
+
+        # Capcity check
+        if len(self.cache) > self.cap:
+            del self.cache[self.left.next.key]
+            self.remove(self.left.next)        
+        
+        
